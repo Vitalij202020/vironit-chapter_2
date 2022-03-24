165338194da89503e97f9f0fae450de63e0d948d
Vitalij202020 <vaskouski.v.v@gmail.com>
2022-03-24 19:34:18 +0300

    second recover

--- task_1/compress.js
+++ task_1/compress.js
@@ -3,3 +3,36 @@ let srt2 = "aaa" // "a3"
 let str3 = "aabbb" // "a2b3"
 let str4 = "aaabcc" // "a3b1c2"
 
+function compress(str) {
+    let count = 1
+    let result
+    const tempArr = []
+    const sourceArr = str.split('')
+
+    if (sourceArr.length === 1) return str
+
+    for (let i = 0; i < sourceArr.length; i++) {
+        let currentValue = sourceArr[i]
+        let nextValue = sourceArr[i + 1]
+
+        if (currentValue === nextValue && i !== sourceArr.length - 1) {
+            count++
+        } else {
+            if (i === sourceArr.length - 1 ) {
+
+            } else {
+                tempArr.push(currentValue)
+                tempArr.push(count)
+                count = 1
+            }
+        }
+    }
+    result = tempArr.join('')
+    console.log(sourceArr)
+    return result
+}
+
+console.log(compress(str1))
+console.log(compress(srt2))
+console.log(compress(str3))
+console.log(compress(str4))
